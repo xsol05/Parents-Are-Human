d15cdbb9b212cf88cad8a609005daf846db0490d
Magdeline Huang <54191678+xsol05@users.noreply.github.com>
2023-04-14 23:54:35 +1200

    Finally got a working state of the app

--- src/App.js
+++ src/App.js
@@ -1,49 +1,39 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import './index.css'
 import './components/Header'
 import Header from './components/Header'
 import Card from './components/Card'
-import Cards from './data/Cards'
+import CardPile from './data/CardPile'
 import Button from './components/Button'
 
 function App() {
   const [level, setLevel] = useState(1)
-  const [cardsPile, setCardsPile] = useState(
-    Cards.filter((card) => card.level === level)
-  )
-  const [cardIndex, setCardIndex] = useState(
-    Math.floor(Math.random() * cardsPile.length)
-  )
+  const [index, setIndex] = useState(0)
 
-  const handleButtonClick = (level) => {
+  const changeLevel = (level) => {
     setLevel(level)
+    setIndex(0) // Reset index to 0 when the level changes so that the index doesn't go out of bounds for the cards of the new level
   }
 
-  useEffect(() => {
-    setCardsPile(Cards.filter((card) => card.level === level))
-    // generateNewCard()
-    console.log('level', level)
-    console.log('cardsPile', cardsPile)
-  }, [level])
-
-  // const generateNewCard = () => {
-  //   setCardIndex(Math.floor(Math.random() * cardsPile.length))
-  // }
+  const generateNewCard = () => {
+    const newIndex = Math.floor(Math.random() * CardPile[level].length)
+    setIndex(newIndex)
+  }
 
   return (
     <>
       <Header />
       <div className="container">
         <div className="buttonArrangement">
-          <Button onClick={() => handleButtonClick(1)} text={'Level 1'} />
-          <Button onClick={() => handleButtonClick(2)} text={'Level 2'} />
-          {/* <Button
+          <Button onClick={() => changeLevel(1)} text={'Level 1'} />
+          <Button onClick={() => changeLevel(2)} text={'Level 2'} />
+          <Button
             onClick={generateNewCard}
             text={'New card'}
             style={{ backgroundColor: '#ff6a95', color: 'rgba(0, 0, 0, 0.4)' }}
-          ></Button> */}
+          ></Button>
         </div>
-        <Card card={cardsPile.filter((card) => card.level === level)} />
+        <Card card={CardPile[level][index]} />
       </div>
     </>
   )
