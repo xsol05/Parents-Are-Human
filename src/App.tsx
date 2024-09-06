8bf663e541c699a4c8215aa8a6c6290363d875fd
Magdeline Huang <54191678+xsol05@users.noreply.github.com>
2024-09-06 15:11:20 +0800

    Remove debugging comments

--- src/App.tsx
+++ src/App.tsx
@@ -31,8 +31,6 @@ const App = () => {
     setUsedCards((prevUsedCards) => [...prevUsedCards, card])
   }
 
-  const [clickCounter, setClickCounter] = useState(1)
-
   // Generate new card:
   // 1. Current card question gets rendered in history list
   // 2. Generate a new card that does not have the same index as any card in history list
@@ -42,11 +40,7 @@ const App = () => {
       return
     }
 
-    setClickCounter((clickCounter) => clickCounter + 1)
-    console.log('✨ Click counter: ', clickCounter)
-
     const oldIndex = index
-    console.log('Old Index: ', oldIndex)
 
     // Generate a new index until it is not present in usedCards
     let newIndex = Math.floor(Math.random() * CardPile[level].length)
@@ -56,13 +50,9 @@ const App = () => {
     ) {
       newIndex = Math.floor(Math.random() * CardPile[level].length)
     }
-    console.log('New Index: ', newIndex)
 
     addUsedCard(CardPile[level][oldIndex])
     setIndex(newIndex)
-    console.log('Old Card: ', CardPile[level][oldIndex])
-    console.log('New Card: ', CardPile[level][newIndex])
-    console.log('Used cards: ', usedCards) // Note that this will not reflect the old card immediately since state updates are asynchronous
   }
 
   return (
